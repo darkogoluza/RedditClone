1f8f79cdfb0774f801a8e374e5823d82c0326ac7
Darko Goluža <darkogoluza@gmail.com>
2022-10-30 14:43:45 +0100

    Log in and out working on client side

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp;
+using BlazorApp.Auth;
 using HttpClients.ClientInterfaces;
 using HttpClients.Implementations;
+using Microsoft.AspNetCore.Components.Authorization;
+using Shared.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,6 +14,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<IPostService, PostHttpClient>();
 builder.Services.AddScoped<ISubFormService, SubFormHttpClient>();
 builder.Services.AddScoped<ICommentService, CommentHttpClient>();
+builder.Services.AddScoped<IAuthService, AuthHttpClient>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthProvider>();
+AuthorizationPolicies.AddPolicies(builder.Services);
+
+
 
 
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri("https://localhost:7207/")});
--- WebAPI/Controllers/AuthController.cs
+++ WebAPI/Controllers/AuthController.cs
@@ -31,6 +31,7 @@ public class AuthController : ControllerBase
             new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
             new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
             new Claim(ClaimTypes.Name, user.UserName),
+            new Claim("Id", user.Id.ToString()),
         };
         return claims.ToList();
     }
